00e73474036a549bdb64e73fdad5f6d4524af346
MehrShahbazAhmad <shahbaz.ahmad@devsinc.com>
2022-08-02 19:26:04 +0500

    Serializer Changes

--- app/serializers/api/v1/item_serializer.rb
+++ app/serializers/api/v1/item_serializer.rb
@@ -5,14 +5,14 @@ module Api
   module V1
     class ItemSerializer < ActiveModel::Serializer
       include Rails.application.routes.url_helpers
-      attributes :id, :title, :description, :price, :url
+      attributes :id, :title, :description, :price, :order_count, :url
       has_many :restaurants
       has_many :categories
 
       def url
         temp = []
         object.images.each do |image|
-          temp << "https://res.cloudinary.com/shahbazdevsinc/image/upload/v1655205984/#{ActiveStorage::Blob.find(image.blob_id).key}.jpeg"
+          temp << Cloudinary::Utils.cloudinary_url(ActiveStorage::Blob.find(image.blob_id).key)
         end
         temp
       end
