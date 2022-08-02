d8a7210067fa0ce8111e0681a6d242f6ecc361b1
MehrShahbazAhmad <shahbaz.ahmad@devsinc.com>
2022-08-02 14:15:02 +0500

    Added Items Images in Urls

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,13 +4,14 @@ module Api
   module V1
     class ItemsController < Api::V1::BaseController
       def index
-        @items = Item.all
-        render json: @items
+        items = Item.all
+        items = items.sort_by_title
+        render json: items
       end
 
       def show
-        @item = Item.find(params[:id])
-        render json: @item
+        item = Item.find(params[:id])
+        render json: item
       end
     end
   end
